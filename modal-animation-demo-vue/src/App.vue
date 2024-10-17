64f1a93cebe8a1828100949b73210d319d921f18
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-10-17 15:48:49 +0800

    build(deps): add @vueuse/motion

--- modal-animation-demo-vue/src/App.vue
+++ modal-animation-demo-vue/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <h1 class="text-3xl font-bold underline" v-motion-fade>Hello world!</h1>
 </template>
